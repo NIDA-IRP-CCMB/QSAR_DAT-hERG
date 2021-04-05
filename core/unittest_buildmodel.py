cadc0090a8e41ad6281c0fe8a94ddc3d4cc6320c
Guan <andyxingjieguan@gmail.com>
2021-04-05 14:52:30 -0400

    update to buildmodel unittest

--- core/unittest_buildmodel.py
+++ core/unittest_buildmodel.py
@@ -138,8 +138,8 @@ class TestBuildModel(unittest.TestCase):
         a, b = calc_phore_descs(train_mols, phore_sigbits, testing=True)
 
         f = open(reference + "/regression_xgb_0.00.log", 'rb')
-        self.assertEquals(f.readlines()[1].replace("\n", ""), a)
-        self.assertEquals(f.readlines()[2].replace("\n", ""), b)
+        self.assertEquals(str(f.readlines()[1].replace("\n", "")), a)
+        self.assertEquals(str(f.readlines()[2].replace("\n", "")), b)
         f.close()
 
     def test_prune_phore_descs(self):
